96adf7086a68a85af09f06ad884bf76b16d4ad2f
jjheffernan <jjheffernan99@gmail.com>
2022-07-22 08:39:58 -0400

    added fractional server backend functions, not using FlaskSocketIO yet.

--- forum/forum_IM/im_server.py
+++ forum/forum_IM/im_server.py
@@ -3,6 +3,12 @@
 """
 import socket
 import threading
+import im_s_receive
+# import time
+# import sys
+import os
+# Set environment variable
+os.environ['TK_SILENCE_DEPRECATION'] = '1'
 
 HOST = '0.0.0.0'  # localhost IP
 PORT = 9090  # port forward to allow LAN communication
@@ -10,5 +16,13 @@ PORT = 9090  # port forward to allow LAN communication
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # internet socket, TCP socket
 server.bind((HOST, PORT))  # setup server on IP address constant above
 
+
+# opens ports to signals
+server.listen()
+
 clients = []  # list of client IPs
 nicknames = []  # list of user aliases
+
+if __name__ == '__main__':
+    print('server running... ')
+    im_s_receive.receive()

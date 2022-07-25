f20ca36eface3f8f667a6630f439c8a966628380
jjheffernan <jjheffernan99@gmail.com>
2022-07-25 00:45:24 -0400

    added setup.py for confid, not for deployment push

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='forum',
-    packages=['forum', 'forum/forum_IM'],
+    packages=['forum/', 'forum/forum_IM'],
     include_package_data=True,
     install_requires=[
         'flask',
